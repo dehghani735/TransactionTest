bb2640402204f944b66ba0650eb62182e7539cb5
Mohammad Reza Dehghani Tafti <dehghani735@gmail.com>
2019-12-11 01:02:32 +0330

    980919 listview added

--- TransactionTest/Form4.Designer.cs
+++ TransactionTest/Form4.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TransactionTest
+namespace TransactionTest
 {
     partial class Form4
     {
@@ -37,17 +37,22 @@
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(467, 12);
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(421, 18);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(259, 288);
+            this.listView1.Size = new System.Drawing.Size(617, 259);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(591, 347);
+            this.button1.Location = new System.Drawing.Point(693, 320);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.Size = new System.Drawing.Size(93, 35);
             this.button1.TabIndex = 1;
             this.button1.Text = "Run";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,9 +61,10 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(577, 407);
+            this.checkBox1.Location = new System.Drawing.Point(421, 362);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(113, 24);
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
             this.checkBox1.TabIndex = 2;
             this.checkBox1.Text = "checkBox1";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -66,29 +72,32 @@
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(28, 23);
+            this.checkedListBox1.Location = new System.Drawing.Point(25, 18);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(403, 277);
+            this.checkedListBox1.Size = new System.Drawing.Size(372, 259);
             this.checkedListBox1.TabIndex = 3;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(577, 470);
+            this.textBox1.Location = new System.Drawing.Point(421, 412);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.Size = new System.Drawing.Size(89, 22);
             this.textBox1.TabIndex = 4;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1220, 614);
+            this.ClientSize = new System.Drawing.Size(1084, 491);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form4";
             this.Text = "Form4";
             this.ResumeLayout(false);
@@ -103,5 +112,5 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.TextBox textBox1;
-    }
+  }
 }
--- TransactionTest/Form4.cs
+++ TransactionTest/Form4.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,81 +11,86 @@ using System.Windows.Forms;
 
 namespace TransactionTest
 {
-    public partial class Form4 : Form
+  public partial class Form4 : Form
+  {
+    public Form4()
     {
-        public Form4()
-        {
-            InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var config = new Config(this);
-            config.ReadFile();
-            config.Parse();
-
-            var planFactory = new PlanFactory(config);
-
-            // TODO: generate plans is incomplete
-            planFactory.GeneratePlans();
-
-            Reporter.Log(System.DateTime.Now.ToString());
-
-            planFactory.ProcessPendingPlans();
-        }
-
-        public void OnTransactionChangeStatus(object source, TransactionEventArgs args)
-        {
-            switch (args.EventType)
-            {
-                case TransactionEventArgs.eStatus.Passed:
-                    textBox1.Text = "Passed";
-                    Console.WriteLine("event Passed handled");
-                    //this.ShowDialog("ddd");
-                    break;
-                case TransactionEventArgs.eStatus.Failed:
-                    textBox1.Text = "Failed";
-                    // streamWriter.WriteLine("====================================================================");
-                    break;
-                default:
-                    break;
-            }
-
-        }
-
-        public void OnItemAdded(object source, TransactionEventArgs args)
-        {
-            switch (args.EventType)
-            {
-                case TransactionEventArgs.eStatus.Passed:
-                    textBox1.Text = "Passed";
-                    Console.WriteLine("event Passed handled");
-                    //this.ShowDialog("ddd");
-                    break;
-                case TransactionEventArgs.eStatus.Failed:
-                    textBox1.Text = "Failed";
-                    // streamWriter.WriteLine("====================================================================");
-                    break;
-                default:
-                    break;
-            }
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs args)
-        {
-            /*switch (args.EventType)
-            {
-                case TransactionEventArgs.eStatus.Passed:
-                    textBox1.Text = "Passed";
-                    break;
-                case TransactionEventArgs.eStatus.Failed:
-                    textBox1.Text = "Failed";
-                    //streamWriter.WriteLine("====================================================================");
-                    break;
-                default:
-                    break;
-            }*/
-        }
+      InitializeComponent();
     }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      var config = new Config(this);
+      config.ReadFile();
+      config.Parse();
+
+      var planFactory = new PlanFactory(config);
+
+      planFactory.PlanAdded += this.OnItemAdded;
+
+      // TODO: generate plans is incomplete
+      planFactory.GeneratePlans();
+
+      Reporter.Log(System.DateTime.Now.ToString());
+
+      planFactory.ProcessPendingPlans();
+    }
+
+    public void OnTransactionChangeStatus(object source, TransactionEventArgs args)
+    {
+      switch (args.EventType)
+      {
+        case TransactionEventArgs.eStatus.Passed:
+          textBox1.Text = "Passed";
+          Console.WriteLine("event Passed handled");
+          //this.ShowDialog("ddd");
+          break;
+        case TransactionEventArgs.eStatus.Failed:
+          textBox1.Text = "Failed";
+          // streamWriter.WriteLine("====================================================================");
+          break;
+        default:
+          break;
+      }
+
+    }
+
+    public void OnItemAdded(object source, PlanEventArgs args)
+    {
+      var lvi = new ListViewItem("salam");
+      lvi.Tag = "salam";
+      listView1.Items.Add(lvi);
+
+      //switch (args.EventType)
+      //{
+      //  case TransactionEventArgs.eStatus.Passed:
+      //    textBox1.Text = "Passed";
+      //    Console.WriteLine("event Passed handled");
+      //    //this.ShowDialog("ddd");
+      //    break;
+      //  case TransactionEventArgs.eStatus.Failed:
+      //    textBox1.Text = "Failed";
+      //    // streamWriter.WriteLine("====================================================================");
+      //    break;
+      //  default:
+      //    break;
+      //}
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs args)
+    {
+      /*switch (args.EventType)
+      {
+          case TransactionEventArgs.eStatus.Passed:
+              textBox1.Text = "Passed";
+              break;
+          case TransactionEventArgs.eStatus.Failed:
+              textBox1.Text = "Failed";
+              //streamWriter.WriteLine("====================================================================");
+              break;
+          default:
+              break;
+      }*/
+    }
+  }
 }
